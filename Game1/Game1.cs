ffac001325281997a5fa5b8706fe9e71dc4d1d20
Jack Calvert <000029140@student.thomroth.ac.uk>
2019-03-27 14:38:10 +0000

    YES

--- Game1/Game1.cs
+++ Game1/Game1.cs
@@ -17,7 +17,7 @@ namespace Game1
         SpriteBatch spriteBatch;
         Vector2 viewportPosition;
         Map Center, Left_Bottom, Bottom_Top, Left_Right, Left_Top, Right_Bottom, Right_Top, map;
-        Map[,] grid = new Map[11, 11];
+        Map[,] grid = new Map[10, 10];
 
         public Game1()
         {
@@ -81,146 +81,275 @@ namespace Game1
             Left[3] = Left_Top;
 
 
-            int[] X_Order = new int[122] ;
+            int[] X_Order = new int[122];
             int[] Y_Order = new int[122];
 
-
+            X_Order[0] = 5;
             X_Order[1] = 6;
-            X_Order[2] = 7;
-            X_Order[3] = 6;
-            X_Order[4] = 5;
+            X_Order[2] = 5;
+            X_Order[3] = 4;
 
+            X_Order[4] = 5;
             X_Order[5] = 6;
             X_Order[6] = 7;
-            X_Order[7] = 8;
-            X_Order[8] = 7;
-            X_Order[9] = 6;
-            X_Order[10] = 5;
+            X_Order[7] = 6;
+            X_Order[8] = 5;
+            X_Order[9] = 4;
+            X_Order[10] = 3;
             X_Order[11] = 4;
-            X_Order[12] = 5;
 
+            X_Order[12] = 5;
             X_Order[13] = 6;
             X_Order[14] = 7;
             X_Order[15] = 8;
-            X_Order[16] = 9;
-            X_Order[17] = 8;
-            X_Order[18] = 7;
-            X_Order[19] = 6;
-            X_Order[20] = 5;
-            X_Order[21] = 4;
+            X_Order[16] = 7;
+            X_Order[17] = 6;
+            X_Order[18] = 5;
+            X_Order[19] = 4;
+            X_Order[20] = 3;
+            X_Order[21] = 2;
             X_Order[22] = 3;
             X_Order[23] = 4;
-            X_Order[24] = 5;
 
+            X_Order[24] = 5;
             X_Order[25] = 6;
             X_Order[26] = 7;
             X_Order[27] = 8;
             X_Order[28] = 9;
-            X_Order[29] = 10;
-            X_Order[30] = 9;
-            X_Order[31] = 8;
-            X_Order[32] = 7;
-            X_Order[33] = 6;
-            X_Order[34] = 5;
-            X_Order[35] = 4;
-            X_Order[36] = 3;
+            X_Order[29] = 8;
+            X_Order[30] = 7;
+            X_Order[31] = 6;
+            X_Order[32] = 5;
+            X_Order[33] = 4;
+            X_Order[34] = 3;
+            X_Order[35] = 2;
+            X_Order[36] = 1;
             X_Order[37] = 2;
             X_Order[38] = 3;
             X_Order[39] = 4;
+            
             X_Order[40] = 5;
-
             X_Order[41] = 6;
             X_Order[42] = 7;
             X_Order[43] = 8;
             X_Order[44] = 9;
             X_Order[45] = 10;
-            X_Order[46] = 11;
-            X_Order[47] = 10;
-            X_Order[48] = 9;
-            X_Order[49] = 8;
-            X_Order[50] = 7;
-            X_Order[51] = 6;
-            X_Order[52] = 5;
-            X_Order[53] = 4;
-            X_Order[54] = 3;
-            X_Order[55] = 2;
+            X_Order[46] = 9;
+            X_Order[47] = 8;
+            X_Order[48] = 7;
+            X_Order[49] = 6;
+            X_Order[50] = 5;
+            X_Order[51] = 4;
+            X_Order[52] = 3;
+            X_Order[53] = 2;
+            X_Order[54] = 1;
+            X_Order[55] = 0;
             X_Order[56] = 1;
             X_Order[57] = 2;
             X_Order[58] = 3;
             X_Order[59] = 4;
-            X_Order[60] = 5;
+
+            X_Order[60] = 6;
+            X_Order[61] = 7;
+            X_Order[62] = 8;
+            X_Order[63] = 9;
+            X_Order[64] = 10;
+            X_Order[65] = 10;
+            X_Order[66] = 9;
+            X_Order[67] = 8;
+            X_Order[68] = 7;
+            X_Order[69] = 6;
+            X_Order[70] = 4;
+            X_Order[71] = 3;
+            X_Order[72] = 2;
+            X_Order[73] = 1;
+            X_Order[74] = 0;
+            X_Order[75] = 0;
+            X_Order[76] = 1;
+            X_Order[77] = 2;
+            X_Order[78] = 3;
+            X_Order[79] = 4;
+
+            X_Order[80] = 7;
+            X_Order[81] = 8;
+            X_Order[82] = 9;
+            X_Order[83] = 10;
+            X_Order[84] = 10;
+            X_Order[85] = 9;
+            X_Order[86] = 8;
+            X_Order[87] = 7;
+            X_Order[88] = 3;
+            X_Order[89] = 2;
+            X_Order[90] = 1;
+            X_Order[91] = 0;
+            X_Order[92] = 0;
+            X_Order[93] = 1;
+            X_Order[94] = 2;
+            X_Order[95] = 3;
+
+            X_Order[96] = 8;
+            X_Order[97] = 9;
+            X_Order[98] = 10;
+            X_Order[99] = 10;
+            X_Order[100] = 9;
+            X_Order[101] = 8;
+            X_Order[102] = 2;
+            X_Order[103] = 1;
+            X_Order[104] = 0;
+            X_Order[105] = 0;
+            X_Order[106] = 1;
+            X_Order[107] = 2;
+
+            X_Order[108] = 9;
+            X_Order[109] = 10;
+            X_Order[110] = 10;
+            X_Order[111] = 9;
+            X_Order[112] = 1;
+            X_Order[113] = 0;
+            X_Order[114] = 0;
+            X_Order[115] = 1;
+
+            X_Order[116] = 10;
+            X_Order[117] = 10;
+            X_Order[118] = 0;
+            X_Order[119] = 0;
 
 
 
 
 
-            Y_Order[1] = 7;
+            Y_Order[0] = 4;
+            Y_Order[1] = 5;
             Y_Order[2] = 6;
             Y_Order[3] = 5;
-            Y_Order[4] = 6;
 
-            Y_Order[5] = 7;
-            Y_Order[6] = 8;
-            Y_Order[7] = 7;
-            Y_Order[8] = 6;
-            Y_Order[9] = 5;
-            Y_Order[10] = 4;
-            Y_Order[11] = 5;
-            Y_Order[12] = 6;
+            Y_Order[4] = 3;
+            Y_Order[5] = 4;
+            Y_Order[6] = 5;
+            Y_Order[7] = 6;
+            Y_Order[8] = 7;
+            Y_Order[9] = 6;
+            Y_Order[10] = 5;
+            Y_Order[11] = 4;
 
-            Y_Order[13] = 7;
-            Y_Order[14] = 8;
-            Y_Order[15] = 9;
-            Y_Order[16] = 8;
+            Y_Order[12] = 2;
+            Y_Order[13] = 3;
+            Y_Order[14] = 4;
+            Y_Order[15] = 5;
+            Y_Order[16] = 6;
             Y_Order[17] = 7;
-            Y_Order[18] = 6;
-            Y_Order[19] = 5;
-            Y_Order[20] = 4;
-            Y_Order[21] = 3;
+            Y_Order[18] = 8;
+            Y_Order[19] = 7;
+            Y_Order[20] = 6;
+            Y_Order[21] = 5;
             Y_Order[22] = 4;
-            Y_Order[23] = 5;
-            Y_Order[24] = 6;
+            Y_Order[23] = 3;
 
-            Y_Order[25] = 7;
-            Y_Order[26] = 8;
-            Y_Order[27] = 9;
-            Y_Order[28] = 10;
-            Y_Order[29] = 9;
-            Y_Order[30] = 8;
-            Y_Order[31] = 7;
+            Y_Order[24] = 1;
+            Y_Order[25] = 2;
+            Y_Order[26] = 3;
+            Y_Order[27] = 4;
+            Y_Order[28] = 5;
+            Y_Order[29] = 6;
+            Y_Order[30] = 7;
+            Y_Order[31] = 8;
             Y_Order[32] = 6;
-            Y_Order[33] = 5;
-            Y_Order[34] = 4;
-            Y_Order[35] = 3;
-            Y_Order[36] = 2;
-            Y_Order[37] = 3;
-            Y_Order[38] = 4;
-            Y_Order[39] = 5;
-            Y_Order[40] = 6;
+            Y_Order[33] = 8;
+            Y_Order[34] = 7;
+            Y_Order[35] = 6;
+            Y_Order[36] = 5;
+            Y_Order[37] = 4;
+            Y_Order[38] = 3;
+            Y_Order[39] = 2;
 
-            Y_Order[41] = 7;
-            Y_Order[42] = 8;
-            Y_Order[43] = 9;
-            Y_Order[44] = 10;
-            Y_Order[45] = 11;
-            Y_Order[46] = 10;
-            Y_Order[47] = 9;
+            Y_Order[40] = 0;
+            Y_Order[41] = 1;
+            Y_Order[42] = 2;
+            Y_Order[43] = 3;
+            Y_Order[44] = 4;
+            Y_Order[45] = 5;
+            Y_Order[46] = 6;
+            Y_Order[47] = 7;
             Y_Order[48] = 8;
-            Y_Order[49] = 7;
-            Y_Order[50] = 6;
-            Y_Order[51] = 5;
-            Y_Order[52] = 4;
-            Y_Order[53] = 3;
-            Y_Order[54] = 2;
-            Y_Order[55] = 1;
-            Y_Order[56] = 2;
+            Y_Order[49] = 9;
+            Y_Order[50] = 10;
+            Y_Order[51] = 9;
+            Y_Order[52] = 8;
+            Y_Order[53] = 7;
+            Y_Order[54] = 6;
+            Y_Order[55] = 5;
+            Y_Order[56] = 4;
             Y_Order[57] = 3;
-            Y_Order[58] = 4;
-            Y_Order[59] = 5;
-            Y_Order[60] = 6;
+            Y_Order[58] = 2;
+            Y_Order[59] = 1;
 
-            
-            bool[,] Connect_Top = new bool [11, 11];
+            Y_Order[60] = 0;
+            Y_Order[61] = 1;
+            Y_Order[62] = 2;
+            Y_Order[63] = 3;
+            Y_Order[64] = 4;
+            Y_Order[65] = 6;
+            Y_Order[66] = 7;
+            Y_Order[67] = 8;
+            Y_Order[68] = 9;
+            Y_Order[69] = 10;
+            Y_Order[70] = 10;
+            Y_Order[71] = 9;
+            Y_Order[72] = 8;
+            Y_Order[73] = 7;
+            Y_Order[74] = 6;
+            Y_Order[75] = 4;
+            Y_Order[76] = 3;
+            Y_Order[77] = 2;
+            Y_Order[78] = 1;
+            Y_Order[79] = 0;
+
+            Y_Order[80] = 0;
+            Y_Order[81] = 1;
+            Y_Order[82] = 2;
+            Y_Order[83] = 3;
+            Y_Order[84] = 7;
+            Y_Order[85] = 8;
+            Y_Order[86] = 9;
+            Y_Order[87] = 10;
+            Y_Order[88] = 10;
+            Y_Order[89] = 9;
+            Y_Order[90] = 8;
+            Y_Order[91] = 7;
+            Y_Order[92] = 3;
+            Y_Order[93] = 2;
+            Y_Order[94] = 1;
+            Y_Order[95] = 0;
+
+            Y_Order[96] = 0;
+            Y_Order[97] = 1;
+            Y_Order[98] = 2;
+            Y_Order[99] = 8;
+            Y_Order[100] = 9;
+            Y_Order[101] = 10;
+            Y_Order[102] = 10;
+            Y_Order[103] = 9;
+            Y_Order[104] = 8;
+            Y_Order[105] = 2;
+            Y_Order[106] = 1;
+            Y_Order[107] = 0;
+
+            Y_Order[108] = 0;
+            Y_Order[109] = 1;
+            Y_Order[110] = 9;
+            Y_Order[111] = 10;
+            Y_Order[112] = 10;
+            Y_Order[113] = 9;
+            Y_Order[114] = 1;
+            Y_Order[115] = 0;
+
+            Y_Order[116] = 0;
+            Y_Order[117] = 10;
+            Y_Order[118] = 10;
+            Y_Order[119] = 0;
+
+
+            bool[,] Connect_Top = new bool[11, 11];
             bool[,] Connect_Right = new bool[11, 11];
             bool[,] Connect_Left = new bool[11, 11];
             bool[,] Connect_Bottom = new bool[11, 11];
@@ -242,29 +371,29 @@ namespace Game1
 
             //-----------------------------------------------------------------------------------
 
-            while (k < 122)
+            while (k < 119)
 
             {
 
-                if (Connect_Bottom[X_Order[k]-1, Y_Order[k + 1]-1] is true)
-                    Connect_Top[X_Order[k]-1, Y_Order[k] - 1] = true;
+                if (Connect_Bottom[X_Order[k], Y_Order[k]] is true)
+                    Connect_Top[X_Order[k], Y_Order[k] - 1] = true;
 
 
 
-                if (Connect_Top[X_Order[k] - 1, Y_Order[k - 1] - 1] is true)
-                    Connect_Bottom[X_Order[k] - 1, Y_Order[k] - 1] = true;
-
-
-
-
-                if (Connect_Left[X_Order[k] - 1, Y_Order[k] - 1] is true)
-                    Connect_Right[X_Order[k - 1] - 1, Y_Order[k] - 1] = true;
+                if (Connect_Top[X_Order[k], Y_Order[k]] is true)
+                    Connect_Bottom[X_Order[k], Y_Order[k] + 1] = true;
 
 
 
 
-                if (Connect_Right[X_Order[k] - 1, Y_Order[k] - 1] is true)
-                    Connect_Left[X_Order[k + 1] - 1, Y_Order[k] - 1] = true;
+                if (Connect_Left[X_Order[k], Y_Order[k]] is true)
+                    Connect_Right[X_Order[k] - 1, Y_Order[k]] = true;
+
+
+
+
+                if (Connect_Right[X_Order[k], Y_Order[k]] is true)
+                    Connect_Left[X_Order[k] + 1, Y_Order[k]] = true;
 
 
                 //---------------------------------------------------------------------------------------------------
@@ -346,8 +475,8 @@ namespace Game1
             }
             //loop end]
 
-            grid[6, 6].ObjectGroups["Events"].Objects["Spawn"].Texture = Content.Load<Texture2D>("sprite");
-            viewportPosition = new Vector2(grid[6, 6].ObjectGroups["Events"].Objects["Spawn"].X - 80, grid[6, 6].ObjectGroups["Events"].Objects["Spawn"].Y - 80);
+            //grid[6, 6].ObjectGroups["Events"].Objects["Spawn"].Texture = Content.Load<Texture2D>("sprite");
+            //viewportPosition = new Vector2(grid[6, 6].ObjectGroups["Events"].Objects["Spawn"].X - 80, grid[6, 6].ObjectGroups["Events"].Objects["Spawn"].Y - 80);
 
 
 
@@ -390,26 +519,26 @@ namespace Game1
             if (keyState.IsKeyDown(Keys.Down))
                 scrolly = -1;
 
-         //   bool action = true;
+            //   bool action = true;
 
-         //   if (keyState.IsKeyDown(Keys.Enter))
-         //   {
-         //      action = false;
-         //       scrollx += gamePadState.ThumbSticks.Left.X;
-         //       scrolly += gamePadState.ThumbSticks.Left.Y;
-         //
-                if (gamePadState.IsButtonDown(Buttons.Back) || keyState.IsKeyDown(Keys.Escape))
-                    this.Exit();
+            //   if (keyState.IsKeyDown(Keys.Enter))
+            //   {
+            //      action = false;
+            //       scrollx += gamePadState.ThumbSticks.Left.X;
+            //       scrolly += gamePadState.ThumbSticks.Left.Y;
+            //
+            if (gamePadState.IsButtonDown(Buttons.Back) || keyState.IsKeyDown(Keys.Escape))
+                this.Exit();
 
-         //       float scrollSpeed = 4.0f;
-
-
-         //       map.ObjectGroups["Events"].Objects["Spawn"].X += (int)(scrollx * scrollSpeed);
-         //       map.ObjectGroups["Events"].Objects["Spawn"].Y -= (int)(scrolly * scrollSpeed);
+            //       float scrollSpeed = 4.0f;
 
 
+            //       map.ObjectGroups["Events"].Objects["Spawn"].X += (int)(scrollx * scrollSpeed);
+            //       map.ObjectGroups["Events"].Objects["Spawn"].Y -= (int)(scrolly * scrollSpeed);
 
-            
+
+
+
 
             base.Update(gameTime);
 
@@ -425,14 +554,14 @@ namespace Game1
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
-            for (int i = 0; i < 11; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int k = 0; k < 11; k++)
+                for (int k = 0; k < 10; k++)
                 {
-                    grid[i,k].Draw(spriteBatch, new Rectangle(i*176, k*176,176,176),Vector2.Zero);
+                    grid[i, k].Draw(spriteBatch, new Rectangle(i * 176, k * 176, 176, 176), Vector2.Zero);
                 }
             }
-            
+
             spriteBatch.End();
 
             base.Draw(gameTime);
